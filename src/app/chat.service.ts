4d5cb14e9562b3af6aa6fa74d20c4572955adbbc
Marcos <marcosgarralaga@gmail.com>
2022-04-10 18:32:24 +0200

    Reformateado chatService

--- src/app/chat.service.ts
+++ src/app/chat.service.ts
@@ -1,48 +1,48 @@
-import { EventEmitter, Injectable } from '@angular/core';  
-import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';  
+import { EventEmitter, Injectable } from '@angular/core';
+import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
 import { Message } from './message';
   
 @Injectable()  
-export class ChatService {  
-  messageReceived = new EventEmitter<Message>();  
-  connectionEstablished = new EventEmitter<Boolean>();  
+export class ChatService {
+  messageReceived = new EventEmitter<Message>();
+  connectionEstablished = new EventEmitter<Boolean>();
   
-  private connectionIsEstablished = false;  
-  private _hubConnection!: HubConnection;  
+  private connectionIsEstablished = false;
+  private _hubConnection!: HubConnection;
   
   constructor() {  
-    this.createConnection();  
-    this.registerOnServerEvents();  
-    this.startConnection();  
+    this.createConnection();
+    this.registerOnServerEvents();
+    this.startConnection();
   }  
   
   sendMessage(message: Message) {  
-    this._hubConnection.invoke('NewMessage', message);  
+    this._hubConnection.invoke('NewMessage', message);
   }  
   
   private createConnection() {  
     this._hubConnection = new HubConnectionBuilder()  
       .withUrl('<DIRECCION URL>/MessageHub')  
-      .build();  
+      .build();
   }  
   
   private startConnection(): void {  
     this._hubConnection  
       .start()  
       .then(() => {  
-        this.connectionIsEstablished = true;  
-        console.log('Hub connection started');  
-        this.connectionEstablished.emit(true);  
+        this.connectionIsEstablished = true;
+        console.log('Hub connection started');
+        this.connectionEstablished.emit(true);
       })  
       .catch(err => {  
-        console.log('Error while establishing connection, retrying...');  
-        setTimeout( () => { this.startConnection(); }, 5000);  
-      });  
+        console.log('Error while establishing connection, retrying...');
+        setTimeout( () => { this.startConnection(); }, 5000);
+      });
   }  
   
   private registerOnServerEvents(): void {  
     this._hubConnection.on('MessageReceived', (data: any) => {  
-      this.messageReceived.emit(data);  
-    });  
+      this.messageReceived.emit(data);
+    });
   }  
 }    
