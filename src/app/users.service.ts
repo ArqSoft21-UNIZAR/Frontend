8d3a2a4fd9ecfb625d14d0bc68c4d9fe67b22532
Marcos <marcosgarralaga@gmail.com>
2022-04-19 17:57:50 +0200

    Fixes userService

--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -12,25 +12,22 @@ export class UsersService {
 
   login(email:string, password:string): Observable<any> {
     return this.http.post("https://meetme-b.herokuapp.com/users/login",
-                          {email: email, password: password});
+                          { email: email,
+                            password: password,
+                            apellidos: "",
+                            localidad: "",
+                            sexo: "",
+                            nombre: "",
+                            meGusta1: "",
+                            meGusta2: "",
+                            meGusta3: "",
+                            noMeGusta1: "",
+                            noMeGusta2: "",
+                            noMeGusta3: "",
+                          });
   }
 
   register(email:string, password:string, name:string, surname:string, gender:string, age:Date, provincia:string, meGusta1:string, meGusta2:string, meGusta3:string, noMeGusta1:string, noMeGusta2:string, noMeGusta3:string): Observable<any> {
-    console.log({
-      email: email,
-      password: password, 
-      nombre: name,
-      apellidos: surname,
-      sexo: gender,
-      fNacimiento: age,
-      localidad: provincia,
-      meGusta1: meGusta1,
-      meGusta2: (meGusta2 != undefined ? meGusta2 :""),
-      meGusta3: (meGusta3 != undefined ? meGusta3 :""),
-      noMeGusta1: noMeGusta1,
-      noMeGusta2: (noMeGusta2 != undefined ? noMeGusta2 :""),
-      noMeGusta3: (noMeGusta3 != undefined ? noMeGusta3 :""),
-    });
     return this.http.post("https://meetme-b.herokuapp.com/users/register",
                           {
                             email: email,
